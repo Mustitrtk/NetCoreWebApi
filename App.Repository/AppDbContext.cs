fc079174cefe95dbaf80bf5fb429b33700d93968
Mustafa Tortuk <mustafat237@hotmail.com>
2025-03-10 04:15:00 +0300

    Proje dosyası ekle.

---
+++ App.Repository/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //Bu proje içerisindeki tüm configure ayarları algılar
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ App.Repository/ProductConfigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class ProductConfigration : IEntityTypeConfiguration<Product>
+    {
+        //Vermiş olduğumuz configration ayarlarını Entity üzerinden vermek kod kirliliğine sebep olur.
+        //Bu nedenle IEntityTypeConfiguration inherit edilen class içerisinden tanımlanıyor.
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(150);
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Stock).IsRequired();
+        }
+    }
+}
