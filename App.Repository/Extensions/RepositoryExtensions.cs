cd86f94403bae3ce69f69c028e1382e64495aa3a
Mustafa Tortuk <mustafat237@hotmail.com>
2025-05-13 14:54:42 +0300

    edited repositories.

--- App.Repository/Extensions/RepositoryExtensions.cs
+++ App.Repository/Extensions/RepositoryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using App.Repository;
+﻿using App.Repositories.Products;
+using App.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +23,10 @@ namespace App.Repositories.Extensions
                     sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
                 });
             });
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             return services;
         }
