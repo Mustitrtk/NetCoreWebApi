79a14ccd2c760f38644d180d43d32ecc712c3d80
Mustafa Tortuk <mustafat237@hotmail.com>
2025-05-13 13:03:37 +0300

    repository added.

--- App.Repository/Migrations/AppDbContextModelSnapshot.cs
+++ App.Repository/Migrations/AppDbContextModelSnapshot.cs
@@ -42,7 +42,7 @@ namespace App.Repositories.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Products");
+                    b.ToTable("Products", (string)null);
                 });
 #pragma warning restore 612, 618
         }
