a643ed10912e65a442b64c8515f49615043f7c91
Mustafa Tortuk <mustafat237@hotmail.com>
2025-05-14 05:59:27 +0300

    edited.

--- App.Services/Products/ProductService.cs
+++ App.Services/Products/ProductService.cs
@@ -9,5 +9,9 @@ namespace App.Services.Products
 {
     public class ProductService(IProductRepository productRepository) : IProductService
     {
+        public Task<List<Product>> GetTopPriceProductAsync(int count)
+        {
+            return productRepository.GetTopPriceProductAsync(count);
+        }
     }
 }
