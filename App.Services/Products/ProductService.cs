47d127f7589f9ebd8ab42a5f4ff9a4caabbcd694
Mustafa Tortuk <mustafat237@hotmail.com>
2025-05-14 20:31:07 +0300

    Service done.

--- App.Services/Products/ProductService.cs
+++ App.Services/Products/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using App.Repositories.Products;
+﻿using App.Repositories;
+using App.Repositories.Products;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,9 @@ using System.Threading.Tasks;
 
 namespace App.Services.Products
 {
-    public class ProductService(IProductRepository productRepository) : IProductService
+    public class ProductService(IProductRepository productRepository, IUnitOfWork unitOfWork) : IProductService
     {
+        //Get Top
         public async Task<ServiceResult<List<ProductDto>>> GetTopPriceProductAsync(int count)
         {
             var products = await productRepository.GetTopPriceProductAsync(count);
@@ -19,16 +21,69 @@ namespace App.Services.Products
             return ServiceResult<List<ProductDto>>.Success(productsAsDto);
         }
 
-        public async Task<ServiceResult<Product>> GetProductByIdAsync(int id)
+        //Get By Id
+        public async Task<ServiceResult<ProductDto>> GetProductByIdAsync(int id)
+        {
+            var product = await productRepository.GetByIdAsync(id);
+
+            if (product is null)
+            {
+                return ServiceResult<ProductDto>.Fail("Product not found", HttpStatusCode.NotFound);
+            }
+
+            var productDto = new ProductDto(product!.Id, product.Name, product.Price, product.Stock);
+
+            return ServiceResult<ProductDto>.Success(productDto);
+        }
+
+        //Create
+        public async Task<ServiceResult<CreateProductResponse>> CreateProductAsync(CreateProductRequest createProduct)
+        {
+            var product = new Product()
+            {
+                Name = createProduct.Name,
+                Price = createProduct.Price,
+                Stock = createProduct.Stock,
+            };
+            await productRepository.AddAsync(product);
+            await unitOfWork.SaveChangesAsync();
+
+            return ServiceResult<CreateProductResponse>.Success(new CreateProductResponse(product.Id));
+        }
+
+        //Update
+        public async Task<ServiceResult> UpdateProductAsync(int id, UpdateProductRequest productRequest)
         {
             var product = await productRepository.GetByIdAsync(id);
 
             if(product is null)
             {
-                return ServiceResult<Product>.Fail("Product not found", HttpStatusCode.NotFound);
+                return ServiceResult.Fail("Data Not Found !",HttpStatusCode.NotFound);
             }
 
-            return ServiceResult<Product>.Success(product);
+            product.Name = productRequest.Name;
+            product.Price = productRequest.Price;
+            product.Stock = productRequest.Stock;
+            
+            productRepository.Update(product);
+            await unitOfWork.SaveChangesAsync();
+
+            return ServiceResult.Success();
+        }
+
+        //Delete
+        public async Task<ServiceResult> DeleteProductAsync(int id)
+        {
+            var product = await productRepository.GetByIdAsync(id);
+
+            if(product is null)
+            {
+                return ServiceResult.Fail("Product Not Found!",HttpStatusCode.NotFound);
+            }
+
+            productRepository.Delete(product);
+            await unitOfWork.SaveChangesAsync();
+            return ServiceResult.Success();
         }
     }
 }
--- App.Services/ServiceResult.cs
+++ App.Services/ServiceResult.cs
@@ -35,4 +35,32 @@ namespace App.Services
             return new ServiceResult<T>() { ErrorMessages = [errorMessage], StatusCode = status };
         }
     }
+
+    public class ServiceResult
+    {
+        public List<string>? ErrorMessages { get; set; }
+
+        public bool IsSuccess => ErrorMessages == null || ErrorMessages.Count == 0;
+
+        public bool IsFail => !IsSuccess;
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        //Static Factory Method
+
+        public static ServiceResult Success(HttpStatusCode status = HttpStatusCode.OK)
+        {
+            return new ServiceResult{ StatusCode = status };
+        }
+
+        public static ServiceResult Fail(List<string> errorMessage, HttpStatusCode status = HttpStatusCode.BadRequest)
+        {
+            return new ServiceResult() { ErrorMessages = errorMessage, StatusCode = status };
+        }
+
+        public static ServiceResult Fail(string errorMessage, HttpStatusCode status = HttpStatusCode.BadRequest)
+        {
+            return new ServiceResult() { ErrorMessages = [errorMessage], StatusCode = status };
+        }
+    }
 }
