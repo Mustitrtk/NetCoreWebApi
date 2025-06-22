a16d5c50856f04bd2fc510930aa12cb3ffa4b67b
Mustafa Tortuk <mustafat237@hotmail.com>
2025-06-22 10:11:04 +0300

    CategoryService added.

---
+++ App.Services/Categories/CategoryService.cs
@@ -0,0 +1,13 @@
+﻿using App.Repositories.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.Categories
+{
+    public class CategoryService(ICategoryRepository categoryRepository):ICategoryService
+    {
+    }
+}
